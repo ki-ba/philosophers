84e5044bb317a40f290b3450d939feb0502d73c8
Killian Barru <kbarru@z1r9p4.42lyon.fr>
2025-07-30 17:11:10 +0200

    fix mutex leak if error on init

--- philo/src/destroy.c
+++ philo/src/destroy.c
@@ -6,7 +6,7 @@
 /*   By: kbarru <kbarru@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 14:28:09 by kbarru            #+#    #+#             */
-/*   Updated: 2025/07/03 18:50:11 by kbarru           ###   ########lyon.fr   */
+/*   Updated: 2025/07/30 17:07:46 by kbarru           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int	destroy_forks(t_fork *forks, int i)
 
 int	destroy_mutexes(t_table *table)
 {
+	pthread_mutex_unlock(&table->start_mut);
 	pthread_mutex_destroy(&table->death);
 	pthread_mutex_destroy(&table->meal_count_mutex);
 	pthread_mutex_destroy(&table->start_mut);
